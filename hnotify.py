d5eb956fb06ed03fdaa1c73e9d8aab13cd5f069a
Gawen <g@wenarab.com>
2012-10-30 10:14:01 +0100

    offline notification

--- hnotify.py
+++ hnotify.py
@@ -27,9 +27,18 @@ logger = logging.getLogger("hnotify")
 def get_state():
     logger.info("Getting state...")
     ndata_elements = 3
-    quantiles = json.loads(urllib.urlopen("http://hnpickup.appspot.com/dm.json?ndata_elements=%s" % (1, )).read())[0]
-    
-    series = json.loads(urllib.urlopen("http://hnpickup.appspot.com/etl.json?ndata_elements=%s" % (ndata_elements, )).read())
+    try:
+        quantiles = json.loads(urllib.urlopen("http://hnpickup.appspot.com/dm.json?ndata_elements=%s" % (1, )).read())[0]
+        
+        series = json.loads(urllib.urlopen("http://hnpickup.appspot.com/etl.json?ndata_elements=%s" % (ndata_elements, )).read())
+
+    except IOError:     # Can't connect
+        logger.warning("Looks like HNPickup is down or no connectivity.")
+        return "offline"
+
+    except ValueError:  # Bad JSON
+        logger.warning("Bad JSON returned.")
+        return "offline"
 
     stories = series.pop()
     data_length = len(series[0]["data"]) - 1
@@ -48,6 +57,8 @@ def get_state():
         return "bad"
 
 class MainWindow(QMainWindow):
+    TIMER_LAPS = 15 * 60 * 1000
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -57,6 +68,7 @@ class MainWindow(QMainWindow):
         self.icon_good = QIcon(os.path.join(local_path, "icon-good.ico"))
         self.icon_soso = QIcon(os.path.join(local_path, "icon-soso.ico"))
         self.icon_bad = QIcon(os.path.join(local_path, "icon-bad.ico"))
+        self.icon_offline = QIcon(os.path.join(local_path, "icon-offline.ico"))
 
         self.refresh_contextmenu_action = QAction("Refresh now", self)
         self.connect(self.refresh_contextmenu_action, SIGNAL("triggered()"), lambda: self.update_pickup(True))
@@ -85,7 +97,7 @@ class MainWindow(QMainWindow):
 
         self.timer = QTimer()
         self.connect(self.timer, SIGNAL("timeout()"), self.update_pickup)
-        self.timer.start(15 * 60 * 1000)
+        self.timer.start(self.TIMER_LAPS)
 
         self.update_pickup()
 
@@ -105,16 +117,18 @@ class MainWindow(QMainWindow):
         if not force_display and self.last_sugg == sugg:
             return
 
-        icon = {
-            "very good": self.icon_verygood,
-            "good": self.icon_good,
-            "so-so": self.icon_soso,
-            "bad": self.icon_bad,
+        icon, message, timer_laps = {
+            "very good": (self.icon_verygood, "Very-good time to post on HN.", None),
+            "good": (self.icon_good, "Good time to post on HN.", None),
+            "so-so": (self.icon_soso, "So-so time to post on HN.", None),
+            "bad": (self.icon_bad, "Bad time to post on HN.", None),
+            "offline": (self.icon_offline, "Offline", 30 * 1000),
+
         }.get(sugg, self.icon_bad)
 
         self.tray.setIcon(icon)
-        
-        self.tray.showMessage("Hacker News", "%s time to post on HN." % (sugg.capitalize(), ))
+        self.timer.setInterval(timer_laps or self.TIMER_LAPS)
+        self.tray.showMessage("Hacker News", message)
         
         self.last_sugg = sugg
             
